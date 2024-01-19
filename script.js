ad17b969010cb2a292fa098b7f858157dd496f52
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-01-19 23:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+let nameValue = document.getElementById("name").value;
+let yearValue = document.getElementById("year").value;
+let submitBtn =  document.getElementById("button");
+let heading = document.getElementById("url");
+
+submitBtn.addEventListener("click", () => {
+    heading.innerText = `https://localhost:8080/?name=${nameValue}&year=${yearValue}`;
+});
+
